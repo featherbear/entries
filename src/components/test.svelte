3791f59377a182621c110ee478b03d1c52cad97f
Andrew Wong <andrew.j.wong@outlook.com>
2022-09-02 02:45:25 +1000

    'touch'

---
+++ src/components/test.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    interface as {
+        b: 23
+    }
+  type A = {
+    s: string;
+  };
+
+  let B: A = {
+    s: "asfd",
+  };
+
+  let c = 0;
+  console.log("RAN TEST CODE");
+  function adad() {
+    console.log("test");
+    c++
+  }
+</script>
+
+hey!
+{B.s}
+{c}
+<button on:click={adad}>GO</button>
